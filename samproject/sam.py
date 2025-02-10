eca4ae5a29c930463962fe25f8d808ac6c5bcbe5
dev sys <skooncode@gmail.com>
2025-02-10 07:23:20 +0530

    initial commit

---
+++ samproject/sam.py
@@ -0,0 +1,5 @@
+colors=["red","blue","yellow","black","white","purple"]
+ascending_orders=sorted(colors)
+print("colors in ascending orders",ascending_orders)
+descending_order=sorted(colors,reverse=True)
+print("colors in descending orders",descending_order)
