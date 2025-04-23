3259be9ff457cd9142eeb746765702dfccc655c5
dev sys <skooncode@gmail.com>
2025-04-23 13:31:29 +0530

    initial commit

--- samproject/sam.py
+++ samproject/sam.py
@@ -1,5 +1,3 @@
-colors=["red","blue","yellow","black","white","purple"]
-ascending_orders=sorted(colors)
-print("colors in ascending orders",ascending_orders)
-descending_order=sorted(colors,reverse=True)
-print("colors in descending orders",descending_order)
+def reverse_number(n):
+    return int(str(n)[::-1])
+print(reverse_number(123456))
